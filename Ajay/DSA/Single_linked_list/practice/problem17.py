40f193a0b4055c26a346d27fe8fc7c8bc553ff2f
ajaybhai7 <squadterbo@gmail.com>
2026-07-14 23:00:29 +0530

    Auto-commit: 2026-07-14 23:00:29

--- Ajay/DSA/Single_linked_list/practice/problem17.py
+++ Ajay/DSA/Single_linked_list/practice/problem17.py
@@ -2,22 +2,21 @@
 
 tasks = []
 
-# --- APP START HOTE HI FILE SE DATA LIST MEIN DAALNA ---
 try:
-    with open("To_do_list.txt", "r") as f:
-        saari_lines = f.readlines()
-        for line in saari_lines:
+    with open("To_do_list.txt" 'r') as f:
+        sari_line = f.readlines()
+        for line in sari_line:
             tasks.append(line.strip())
 except FileNotFoundError:
-    pass 
+    pass
 
-# --- SHORTCUT FUNCTION (List ko file me save karne ke liye) ---
 def save_to_file():
-    with open("To_do_list.txt", "w") as f:
+    with open("To_do_list.txt", 'w') as f:
         for t in tasks:
             f.write(t + "\n")
 
-# --- MAIN APP LOOP ---
+
+
 while True:
     try:
         print("\n1. Add task\n2. View task\n3. Delete task\n4. Exit")
@@ -26,15 +25,16 @@ while True:
         if user_choose == 1:
             naya_task = input("Enter Task to add: ")
             tasks.append(naya_task)
-            save_to_file() # Shortcut use kiya
+            save_to_file()
 
         elif user_choose == 2:
             if len(tasks) == 0:
                 print("To do list khali hai!")
+
             else:
                 for task in tasks:
                     print(f"- {task}")
-
+                
         elif user_choose == 3:
             if len(tasks) == 0:
                 print("List khali hai!")
@@ -46,11 +46,9 @@ while True:
                     print(f"Successfully removed '{remove_task}'")
                 else:
                     print("Task nahi mila!")
-
         elif user_choose == 4:
             break
-
     except ValueError:
         print("Please enter a valid number!")
-
 print("Thanks for using!")
+
