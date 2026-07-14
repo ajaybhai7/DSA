2a4712241d2bd640cd513fbc806ae9dff6767dc6
ajaybhai7 <squadterbo@gmail.com>
2026-07-14 21:17:52 +0530

    Auto-commit: 2026-07-14 21:17:52

--- Ajay/DSA/Single_linked_list/practice/problem16.py
+++ Ajay/DSA/Single_linked_list/practice/problem16.py
@@ -13,25 +13,42 @@ while True:
             user_choose = int(input("Taks -> ").lower())
 
             if user_choose == 1:
-                tasks.append(input("Enter Task to add: "))
+                to_do = tasks.append(input("Enter Task to add: "))
+                with open("To_do_list.txt", 'w')as f:
+                     f.write(to_do)
 
             elif user_choose == 2:
+                    
+                    with open("To_do_list.txt", 'r') as r:
+                         read = r.read()
 
-                    if len(tasks) == 0: 
+                    if read == "0": 
                         print("To do list khali hai !")
 
                     else:
-                        for task in tasks:
-                            print(f"Your tasks are\n{tasks}")
+                        with open("To_do_list.txt", 'r') as p:
+                             to_do_print = p.read()
+                             to_do_print = p.readlines()
+                        for task in to_do_print:
+                            print(f"Your tasks are\n{to_do_print}")
 
             elif user_choose == 3:
-                if len(tasks) == 0:
+
+                with open ("To_do_list.txt", 'r') as l:
+                     to_do_read_blank = l.read()
+
+                if  to_do_read_blank == "":
                      print("To do list Khali hai !")
                 
                 
                 else:
                     remove_task = input("Enter to remove task: ")
-                    if (remove_task) in tasks:
+                    with open ("To_do_list.txt", 'a') as a:
+                         append = a.write(remove_task)
+                    
+                    with open("To_do_list.txt", 'r') as x:
+                         to_do_remove = x.read()
+                    if (remove_task) in to_do_remove:
                         tasks.remove(remove_task)
                         print(f"Successfully remove {remove_task} task in to do list")
                 
@@ -44,5 +61,4 @@ while True:
         except ValueError:
              print("Enter Corect argument or Value")  
 print("Thanks for using") 
-        
     
