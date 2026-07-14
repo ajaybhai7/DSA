b2e525fab71e1681c2a72e28d9eb2dbb0c39ad0b
ajaybhai7 <squadterbo@gmail.com>
2026-07-14 20:28:05 +0530

    Auto-commit: 2026-07-14 20:28:05

--- Ajay/DSA/Single_linked_list/practice/problem16.py
+++ Ajay/DSA/Single_linked_list/practice/problem16.py
@@ -1,32 +1,34 @@
 # To do list
-try:
-    tasks = []
+tasks = []
 
-    while True:
+while True:
+        try:
 
-        print('''
-    1. Add task
-    2. view task
-    3. Delete task
-    4. Exit''')
+            print('''
+        1. Add task
+        2. view task
+        3. Delete task
+        4. Exit''')
+            
+            user_choose = int(input("Taks -> ").lower())
+
+            if user_choose == 1:
+                tasks.append(input("Enter Task to add: "))
+
+            elif user_choose == 2:
+                for task in tasks:
+                    print(f"Your tasks are\n{tasks}")
+
+            elif user_choose == 3:
+                remove_task = input("Enter to remove task: ").lower()
+                tasks.remove(remove_task)
+                print(f"Successfully remove {remove_task} task in to do list")
+
+            else:
+                break
+            print("Thanks for using") 
+
+        except ValueError:
+             print("Enter Corect argument or Value")  
         
-        user_choose = int(input("Taks -> ").lower())
-
-        if user_choose == 1:
-            tasks.append(input("Enter Task to add: "))
-
-        elif user_choose == 2:
-            for task in tasks:
-                print(f"Your tasks are\n{tasks}")
-
-        elif user_choose == 3:
-            remove_task = input("Enter to remove task: ").lower()
-            tasks.remove(remove_task)
-            print(f"Successfully remove {remove_task} task in to do list")
-
-        else:
-            break
-    print("Thanks for using")
-
-except ValueError:
-    print("PLease Give Correct argument")
+    
