e6cc5cd12824068cf2797a06cc84c80edbe54fed
ajaybhai7 <squadterbo@gmail.com>
2026-07-14 19:21:20 +0530

    Auto-commit: 2026-07-14 19:21:20

---
+++ Ajay/DSA/Single_linked_list/practice/probelem12.py
@@ -0,0 +1,13 @@
+# Write a function to repete a word 
+
+def repeat_word(word, repete):
+
+    final = word*repete
+    return final
+
+word = input("Enter a Word: ")
+repeat = input("How times to repeat: ")
+
+dohrana = repeat_word(word, repeat)
+
+print(dohrana)
