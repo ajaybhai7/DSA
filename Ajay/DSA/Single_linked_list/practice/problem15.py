933705a1824de49dcf06cc82e94b7abfce4e6c72
ajaybhai7 <squadterbo@gmail.com>
2026-07-14 19:56:24 +0530

    Auto-commit: 2026-07-14 19:56:24

--- Ajay/DSA/Single_linked_list/practice/problem15.py
+++ Ajay/DSA/Single_linked_list/practice/problem15.py
@@ -4,16 +4,20 @@ from random import randint
 
 secret_number = randint(1, 10)
 
+total_guess = 0
 
 while True:
-    user_guess = int(input("Guess a Number"))
+    user_guess = int(input("Guess a Number: "))
 
     if user_guess < secret_number:
         print("Lower Number")
-        
+        total_guess += 1
+
     elif user_guess > secret_number:
         print("Higer Number")
+        total_guess +=1
 
     if user_guess == secret_number:
-        print("You guess the number")
+        print(f"You guess the number\nyou guess in : {total_guess} Attemp")
+        total_guess +=1
         break
