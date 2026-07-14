d7fc8fae2a424927cc3a8216c03cc9216868d466
ajaybhai7 <squadterbo@gmail.com>
2026-07-14 22:06:30 +0530

    Auto-commit: 2026-07-14 22:06:29

--- Ajay/DSA/Single_linked_list/practice/problem16.py
+++ Ajay/DSA/Single_linked_list/practice/problem16.py
@@ -1,70 +1,69 @@
-# # To do list
-# tasks = []
+# To do list
+tasks = []
 
-# while True:
-#         try:
+while True:
+        try:
 
-#             print('''
-#         1. Add task
-#         2. view task
-#         3. Delete task
-#         4. Exit''')
+            print('''
+        1. Add task
+        2. view task
+        3. Delete task
+        4. Exit''')
             
-#             user_choose = int(input("Taks -> ").lower())
+            user_choose = int(input("Taks -> ").lower())
 
-#             if user_choose == 1:
-#                 to_do = (input("Enter Task to add: "))
-#                 with open("To_do_list.txt", 'w')as f:
-#                      f.write(to_do)
+            if user_choose == 1:
+                to_do = (input("Enter Task to add: "))
+                with open("To_do_list.txt", 'w')as f:
+                     f.write(to_do)
 
-#             elif user_choose == 2:
+            elif user_choose == 2:
                     
-#                     with open("To_do_list.txt", 'r') as file:
-                         
+                    with open("To_do_list.txt", 'f') as file:
+                        file = f.read()
+                        to_do_print = file.readlines()
+                        if file == "": 
+                            print("To do list khali hai !")
 
-#                         if file.read() == " ": 
-#                             print("To do list khali hai !")
+                        else:
+                            for task in to_do_print:
+                                print(f"Your tasks are\n{to_do_print}")
 
-#                         else:
-#                             to_do_print = file.readlines()
-#                             for task in to_do_print:
-#                                 print(f"Your tasks are\n{to_do_print}")
+            elif user_choose == 3:
 
-#             elif user_choose == 3:
+                with open ("To_do_list.txt", 'r') as l:
+                     to_do_read_blank = l.read()
 
-#                 with open ("To_do_list.txt", 'r') as l:
-#                      to_do_read_blank = l.read()
-
-#                 if  to_do_read_blank == "":
-#                      print("To do list Khali hai !")
+                if  to_do_read_blank == "":
+                     print("To do list Khali hai !")
                 
                 
-#                 else:
-#                     remove_task = input("Enter to remove task: ")
-#                     with open ("To_do_list.txt", 'w') as a:
-#                          append = a.write(remove_task)
+                else:
+                    remove_task = input("Enter to remove task: ")
+                    with open ("To_do_list.txt", 'w') as a:
+                         append = a.write(remove_task)
                     
-#                     with open("To_do_list.txt", 'r') as x:
-#                          to_do_remove = x.read()
-#                     if (remove_task) in to_do_remove:
-#                         tasks.remove(remove_task)
-#                         print(f"Successfully remove {remove_task} task in to do list")
+                    with open("To_do_list.txt", 'r') as x:
+                         to_do_remove = x.read()
+                    if (remove_task) in to_do_remove:
+                        tasks.remove(remove_task)
+                        print(f"Successfully remove {remove_task} task in to do list")
                 
-#                     else:
-#                         print("Remove task list me nahi mila")
+                    else:
+                        print("Remove task list me nahi mila")
 
-#             else:
-#                 break
+            else:
+                break
 
-#         except ValueError:
-#              print("Enter Corect argument or Value")  
-# print("Thanks for using") 
+        except ValueError:
+             print("Enter Corect argument or Value")  
+print("Thanks for using") 
     
-with open("To_do_list.txt") as f:
-    file = f.read()
+# with open("To_do_list.txt") as f:
+#     file = f.read()
 
-    if file == "":
-        print("File is empty")
+#     if file == "":
+#         print("File is empty")
     
-    else:
-        print(file)
+#     else:
+#         print(file)
